cfc7f8ce65e240f7ec6db96cdb45625a6c9b11ad
FemtoHell <femtohell@gmail.com>
2025-12-23 21:00:33 +0700

    SMAR-39 Remove generated-routes.ts from gitignore for CI/CD build
    From Jira

---
+++ src/generated-routes.ts
@@ -0,0 +1,9 @@
+// Auto-generated file by generate-routes.js
+import type { RouteObject } from 'react-router'
+
+export const routes: RouteObject[] = [
+  {
+    path: '/',
+    lazy: () => import('./pages/login.sync'),
+  },
+]
