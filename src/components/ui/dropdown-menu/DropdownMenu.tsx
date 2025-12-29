eaa0db51a53e33673e36efb91e68594c9fa103dd
Trang Mai <40815729+trangmaiq@users.noreply.github.com>
2025-12-29 17:00:51 +0700

    fix(SMAR-14): drop down z-index (#29)

--- src/components/ui/dropdown-menu/DropdownMenu.tsx
+++ src/components/ui/dropdown-menu/DropdownMenu.tsx
@@ -60,7 +60,7 @@ const DropdownMenuSubContent = ({
   <DropdownMenuPrimitive.SubContent
     ref={ref}
     className={clsxm(
-      'bg-material-medium backdrop-blur-background text-text z-60min-w-32 overflow-hidden rounded-[6px] border border-border p-1',
+      'bg-material-medium backdrop-blur-background text-text z-50 min-w-32 overflow-hidden rounded-[6px] border border-border p-1',
       'shadow-context-menu',
       'data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-95 data-[side=bottom]:slide-in-from-top-2 data-[side=left]:slide-in-from-right-2 data-[side=right]:slide-in-from-left-2 data-[side=top]:slide-in-from-bottom-2',
       className,
@@ -84,7 +84,7 @@ const DropdownMenuContent = ({
       ref={ref}
       sideOffset={sideOffset}
       className={clsxm(
-        'bg-material-medium backdrop-blur-background text-text z-60min-w-32 overflow-hidden rounded-[6px] border border-border p-1',
+        'bg-material-medium backdrop-blur-background text-text z-50 min-w-32 overflow-hidden rounded-[6px] border border-border p-1',
         'shadow-context-menu',
         'data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-95 data-[side=bottom]:slide-in-from-top-2 data-[side=left]:slide-in-from-right-2 data-[side=right]:slide-in-from-left-2 data-[side=top]:slide-in-from-bottom-2',
         className,
